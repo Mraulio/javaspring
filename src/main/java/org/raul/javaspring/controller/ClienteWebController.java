75f10e87382202d1b258ab65287160c8c1f516bc
Raul M <raulmvidal87@gmail.com>
2025-10-04 16:06:21 +0200

    feat: buscador en pantalla de listar

--- src/main/java/org/raul/javaspring/controller/ClienteWebController.java
+++ src/main/java/org/raul/javaspring/controller/ClienteWebController.java
@@ -4,10 +4,9 @@ import org.raul.javaspring.entity.Cliente;
 import org.raul.javaspring.repository.ClienteRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/web/clientes")
@@ -36,4 +35,28 @@ public class ClienteWebController {
         clienteRepository.save(cliente);
         return "redirect:/web/clientes";
     }
+
+    @GetMapping("/buscar")
+    public String buscarClientes(
+            @RequestParam(required = false) String nombre,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String telefono,
+            Model model
+    ) {
+        List<Cliente> clientes;
+
+        if (nombre != null && !nombre.isEmpty()) {
+            clientes = clienteRepository.findByNombreContainingIgnoreCase(nombre);
+        } else if (email != null && !email.isEmpty()) {
+            clientes = clienteRepository.findByEmailContainingIgnoreCase(email);
+        } else if (telefono != null && !telefono.isEmpty()) {
+            clientes = clienteRepository.findByTelefonoContainingIgnoreCase(telefono);
+        } else {
+            clientes = clienteRepository.findAll();
+        }
+
+        model.addAttribute("clientes", clientes);
+        return "clientes/listar";
+    }
+
 }
--- src/main/java/org/raul/javaspring/repository/ClienteRepository.java
+++ src/main/java/org/raul/javaspring/repository/ClienteRepository.java
@@ -3,8 +3,14 @@ package org.raul.javaspring.repository;
 import org.raul.javaspring.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>, ClienteRepositoryCustom {
     Optional<Cliente> findByEmail(String email);
+
+    // Se generan automaticamente con Spring Data
+    List<Cliente> findByNombreContainingIgnoreCase(String nombre);
+    List<Cliente> findByEmailContainingIgnoreCase(String email);
+    List<Cliente> findByTelefonoContainingIgnoreCase(String telefono);
 }
